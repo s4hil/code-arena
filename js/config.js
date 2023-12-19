209caecfe082b2ee100950fc736930d75a442aa0
s4hil <s4hilp4rr4y@gmail.com>
2023-12-20 00:32:11 +0530

    v2

--- js/config.js
+++
@@ -1,4 +0,0 @@
-var SAVE_DATA = "http://localhost/code-arena-api/saveData.php";
-var UPLOAD_DIR = "http://localhost/code-arena-api/uploads/";
-var GET_DATA = "http://localhost/code-arena-api/getData.php";
-var QR_URL = "http://localhost/code-arena-api/getQR.php";
