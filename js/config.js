8fc013c13993622180b351e892f71c4d56bfc10c
s4hil <s4hilp4rr4y@gmail.com>
2023-12-21 01:44:17 +0530

    made everything less ugly

---
+++ js/config.js
@@ -0,0 +1,6 @@
+var SAVE_DATA = "http://localhost/code-arena-api/saveData.php";
+var UPLOAD_DIR = "http://localhost/code-arena-api/uploads/";
+var QR_URL = "http://localhost/code-arena-api/getQR.php";
+
+var GET_SUBMISSIONS_URL = "http://localhost/code-arena-api/getData.php";
+var DEL_SUBMISSION_URL = "http://localhost/code-arena-api/delSubmission.php";
