ba7857c47e82b72425cc9d97fcefa7032e32fdbc
s4hil <s4hilp4rr4y@gmail.com>
2024-01-13 04:17:31 +0530

    url redirection

--- js/config.js
+++ js/config.js
@@ -6,3 +6,5 @@ var QR_URL = BASE_URL+"getQR.php";
 
 var GET_SUBMISSIONS_URL = BASE_URL+"getData.php";
 var DEL_SUBMISSION_URL = BASE_URL+"delSubmission.php";
+var GET_ALL_PARTICIPANTS = "http://localhost/code-arena-api/getAllParticipants.php";
+var GET_ALL_TEAMS = "http://localhost/code-arena-api/getAllTeams.php";
